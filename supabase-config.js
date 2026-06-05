f8a18973e566552f5f0035b130d1efb55c85f33b
jiguang121 <128281563+jiguang121@users.noreply.github.com>
2026-06-05 09:52:18 +0800

    Switch Supabase config to legacy anon key

--- supabase-config.js
+++ supabase-config.js
@@ -1,4 +1,4 @@
 window.GC_SUPABASE_CONFIG = {
   url: 'https://cznolglidefgpovnehd.supabase.co',
-  anonKey: 'sb_publishable_gP6e-70etM1OBx-Od9eX9w_-yOotEsQ'
+  anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNuem5vbGdsaWRlZmdwb3ZuZWhkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODA1OTI0ODYsImV4cCI6MjA5NjE2ODQ4Nn0.-W0ciBaeapnpfYHqHDFu6jhZy6kyZXpIoUlkOJ-GCoI'
 };
